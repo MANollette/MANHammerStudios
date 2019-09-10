bc28739908e63253d8070269cd33addc423686e5
Michael Nollette <michael.a.nollette@gmail.com>
2019-09-10 05:44:42 -0700

    Incorporated modals for example images. Fine-tuned margins.

--- MANHammerStudios/Scripts/tabControl.js
+++ MANHammerStudios/Scripts/tabControl.js
@@ -1,20 +1,20 @@
 
 
 
-var modal = document.getElementById('freebladeModal');
+var fbModal = document.getElementById('freebladeModal');
 
 var img = document.getElementById('FreebladeEx');
 var modalImg = document.getElementById("freeblade01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
-  modal.style.display = "block";
+  fbModal.style.display = "block";
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
 }
 
 var span = document.getElementsByClassName("close")[0];
 span.onclick = function () {
-  modal.style.display = "none";
+  fbModal.style.display = "none";
 }
 
 var modal2 = document.getElementById('flamerModal');
